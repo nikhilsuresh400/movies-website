35b0d5d52c4b3b574e2a83771cf0835e71c7b427
Nikhil Suresh <nikaynm400@gmail.com>
2026-02-11 21:38:19 +0530

    feat: implement MovieSlider with dynamic movie data and enhance MovieContent layout

--- src/components/MovieContent.jsx
+++ src/components/MovieContent.jsx
@@ -3,13 +3,21 @@ import HeroSection from './HeroSection'
 import MovieSlider from './MovieSlider'
 import GenreSection from './GenreSection'
 import MoviesDetails from './MoviesDetails'
+import { useMovies } from '../context/MoviesContext'
 
 const MovieContent = () => {
+    const { trendingMovies } = useMovies();
+
     return (
         <>
             <HeroSection />
             <div className='bg-linear-to-b from-bg-dark to-black'>
-                <MovieSlider />
+                <MovieSlider
+                    title="Trending This Week"
+                    subtitle="Stay updated with what everyone's watching"
+                    movies={trendingMovies}
+                    id = "trending"
+                />
                 <GenreSection />
             </div>
 
